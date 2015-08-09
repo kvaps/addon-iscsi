461987cf5ab4742e9306bcc81dfeddb3ec9bc8d9
Tao Zhang <zhtao@cs.stonybrook.edu>
2015-08-09 01:45:16 -0400

    Add script for get image list

---
+++ additional_scripts/get-datastore-images.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+import os
+import xml.etree.ElementTree as ET
+
+oneimage = os.popen("oneimage list --xml")
+
+tree = ET.fromstring(oneimage.read())
+
+print(tree.tag)
+
+for image in tree.findall('./IMAGE'):
+    imageid = image.find('./ID')
+    print(imageid.text)
+    for vmid in image.findall('./VMS/ID'):
+        print(imageid.text, vmid.text)
+
---
+++ additional_scripts/get-zfs-images.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+su oneadmin -c "ssh 10.10.0.15 'sudo zfs list'" | grep "cloud/opennebula/persistent/lv-one-" | awk '{ print $1 }' | sed 's/cloud\/opennebula\/persistent\/lv-one-//' | sed 's/-/ /'
+
