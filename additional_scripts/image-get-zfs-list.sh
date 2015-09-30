a7e2afd8ae0d147336b867a3a2bbe4f681638926
Tao Zhang <zhtao@cs.stonybrook.edu>
2015-09-30 11:22:58 -0400

    Update script to fit new name pattern

--- additional_scripts/image-get-zfs-list.sh
+++ additional_scripts/image-get-zfs-list.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ssh 10.10.0.15 'sudo zfs list' | grep "cloud/opennebula/persistent/lv-one-" | awk '{ print $1 }' | sed 's/cloud\/opennebula\/persistent\/lv-one-//' | sed 's/-/ /'
+ssh 10.10.0.15 'sudo zfs list' | grep "cloud/opennebula/persistent/lv-one-" | awk '{ print $1 }' | sed 's/cloud\/opennebula\/persistent\/lv-one-//' | sed 's/-/ /g' | awk '{ if (NF == 1) print $1; else print $1 " " $2 }'
 
